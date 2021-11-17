8e30460c25d5dced230448d57d333d4a970011b1
SandraCabrera <sandracabreragarcia26@gmail.com>
2021-11-17 16:09:23 +0100

    Casos test del constructor de bascula

--- test/bascula_test.js
+++ test/bascula_test.js
@@ -1,6 +1,38 @@
-const assert = require('assert');
-const Bascula = require('../bascula.js');
+var assert = require('assert');
+var expect = require('chai').expect;
+var Bascula = require('../bascula.js');
 
 describe('Bascula', () => {
-  
+  /**
+   * Casos test del constructor.
+   */
+  it ('El contructor devuelve un objeto con las variables pesos, altura, fechas y anotaciones inicializadas.', function(){
+    var actualResult = Bascula.constructor();
+    var expectedResult = {
+      pesos: [],
+      alturas: [],
+      fechas: [],
+      anotaciones: 0,
+    };
+    
+    expect(actualResult).to.eql(expectedResult);
+  });
+
+  it ('El contructor no debe devolver un objeto vacio.', function(){
+    var actualResult = Bascula.constructor();
+    var expectedResult = {};
+    
+    expect(actualResult).to.not.eql(expectedResult);
+  });
+
+  it ('Que no le falte ningún valor al objeto que devuelve el constructor.', function(){
+    var actualResult = Bascula.constructor();
+    var expectedResult = {
+      pesos: [],
+      alturas: [],
+      fechas: [],
+    };
+    
+    expect(actualResult).to.not.eql(expectedResult);
+  });
 });
